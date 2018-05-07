1f02df6719d7db20607fa4d0b5a2f3c9550d77a3
chenlei <chenlei@likingfit.com>
2018-05-08 00:53:21 +0800

    update

--- app/src/main/java/movies/lchen/com/runway2view/RoadView2.java
+++ app/src/main/java/movies/lchen/com/runway2view/RoadView2.java
@@ -36,6 +36,12 @@ public class RoadView2 extends View {
 
     private int mDashPhase;
 
+    private float mRoalOffset;
+
+    private float mRoalOffsetLeft;
+
+    private float mRoalOffsetRight;
+
     /**
      * 虚线段长度
      */
@@ -53,26 +59,45 @@ public class RoadView2 extends View {
     private boolean isRunning = false;
 
     private ObjectAnimator animator = ObjectAnimator.ofInt(this, "phase", 0, mDashMinSize);
+
     private Animator.AnimatorListener mAnimatorListener = new Animator.AnimatorListener() {
         @Override
         public void onAnimationStart(Animator animation) {
+            animator2.start();
+            animator3.start();
+            animator4.start();
         }
 
         @Override
         public void onAnimationEnd(Animator animation) {
+            animator2.end();
+            animator3.end();
+            animator4.end();
         }
 
         @Override
         public void onAnimationCancel(Animator animation) {
+            animator2.cancel();
+            animator3.cancel();
+            animator4.cancel();
         }
 
         @Override
         public void onAnimationRepeat(Animator animation) {
             if (!isRunning) {
                 animation.cancel();
+                animator2.cancel();
+                animator3.cancel();
+                animator4.cancel();
             }
         }
     };
+
+    private ObjectAnimator animator2 = ObjectAnimator.ofFloat(this, "roalOffset", 0.4f, 0.7f, 0.4f);
+
+    private ObjectAnimator animator3 = ObjectAnimator.ofFloat(this, "roalOffsetLeft", 0.1f, 0.4f, 0.1f);
+
+    private ObjectAnimator animator4 = ObjectAnimator.ofFloat(this, "roalOffsetRight", 0.7f, 1, 0.7f);
 
     private int[] mlineColors =
             {Color.parseColor("#1A45C7BD"),
@@ -97,7 +122,6 @@ public class RoadView2 extends View {
 
     private void init() {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setStyle(Paint.Style.STROKE);
 
         mPath = new Path();
 
@@ -105,6 +129,18 @@ public class RoadView2 extends View {
         animator.setInterpolator(new LinearInterpolator());
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.addListener(mAnimatorListener);
+
+        animator2.setDuration(2 * 60 * 1000);
+        animator2.setInterpolator(new LinearInterpolator());
+        animator2.setRepeatCount(ValueAnimator.INFINITE);
+
+        animator3.setDuration(2 * 60 * 1000);
+        animator3.setInterpolator(new LinearInterpolator());
+        animator3.setRepeatCount(ValueAnimator.INFINITE);
+
+        animator4.setDuration(2 * 60 * 1000);
+        animator4.setInterpolator(new LinearInterpolator());
+        animator4.setRepeatCount(ValueAnimator.INFINITE);
     }
 
     @Override
@@ -113,9 +149,6 @@ public class RoadView2 extends View {
 
         mWidth = getWidth();
         mHeight = getHeight();
-
-//        mPath.moveTo(w / 2, 0);
-//        mPath.lineTo(w / 2, h);
 
         if (mDashGradient == null) {
             mDashGradient = new LinearGradient(
@@ -134,18 +167,32 @@ public class RoadView2 extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.save();
+        mPaint.reset();
+        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(6);
+
+        canvas.save();
+        mPath.reset();
+        mPath.moveTo(mWidth * mRoalOffsetLeft, 0);
+        mPath.quadTo(mWidth / 5, mHeight * 0.2f, mWidth / 5, mHeight);
         mPaint.setShader(mRoadLGEdgeLeft);
-        canvas.drawLine(10, 0, 10, mHeight, mPaint);
-        mPaint.setShader(mRoadLGEdgeRight);
-        canvas.drawLine(mWidth - 10, 0, mWidth - 10, mHeight, mPaint);
+        canvas.drawPath(mPath, mPaint);
         canvas.restore();
 
         canvas.save();
-        mPath.quadTo(Math.round(mWidth), 0,mWidth / 2, mHeight);
-//        mPath.lineTo(mWidth / 2, mHeight);
-        mPaint.setStrokeWidth(30);
+        mPath.reset();
+        mPath.moveTo(mWidth * mRoalOffsetRight, 0);
+        mPath.quadTo((mWidth - (mWidth / 5)), mHeight * 0.2f, (mWidth - (mWidth / 5)), mHeight);
+        mPaint.setShader(mRoadLGEdgeRight);
+        canvas.drawPath(mPath, mPaint);
+        canvas.restore();
+
+        canvas.save();
+        mPath.reset();
+        mPath.moveTo(mWidth * mRoalOffset, 0);
+        mPath.quadTo(mWidth / 2, mHeight * 0.2f, mWidth / 2, mHeight);
+        mPaint.setStrokeWidth(20);
         mPaint.setShader(mDashGradient);
         mPaint.setPathEffect(getPathEffect());
         canvas.drawPath(mPath, mPaint);
@@ -157,6 +204,21 @@ public class RoadView2 extends View {
     public void setPhase(int phase) {
         this.mDashPhase = phase;
         invalidate();
+    }
+
+    @SuppressWarnings("unused")
+    public void setRoalOffset(float roalOffset) {
+        this.mRoalOffset = roalOffset;
+    }
+
+    @SuppressWarnings("unused")
+    public void setRoalOffsetLeft(float roalOffsetLeft) {
+        this.mRoalOffsetLeft = roalOffsetLeft;
+    }
+
+    @SuppressWarnings("unused")
+    public void setRoalOffsetRight(float roalOffsetRight) {
+        this.mRoalOffsetRight = roalOffsetRight;
     }
 
     private static PathEffect makeDash(float phase) {
